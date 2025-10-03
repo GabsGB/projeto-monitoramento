57df8d29d86bb8d086177505e018d5626ae96cb5
Gabriel R. <gabrielrodriguesalves1@gmail.com>
2025-10-03 08:45:14 -0300

    finalizado atualizção adicionando as mensagens de log

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from services.scan_service import scan_filiais, scan_bd, attCont_mensais, scan_manual
+from services.scan_service import scan_filiais, scan_bd, attCont_mensais, scan_manual, attImpressora_filial
 from classes.ImpressoraController import ImpressoraController
 from services.bd_service import read_impressoras
 from util.snmp import snmp
 
 if __name__ == "__main__":
+    
+    resp = input("Realizar somente o scan, atualizar contadores?  \n1 - Scan \n2-Scan + Contadores")
+
     scan_filiais()
     impressoras = scan_bd()
-    attCont_mensais(impressoras)
+    attImpressora_filial(impressoras)
+    if resp == '2':
+        attCont_mensais(impressoras)
     
