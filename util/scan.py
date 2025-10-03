3bd3f2bc854e94d0850fe4a87203fc9cb4b9a616
Gabriel R. <gabrielrodriguesalves1@gmail.com>
2025-10-03 08:38:49 -0300

    finalizado atualizção do código do /util adicionando as mensagens de log

--- util/scan.py
+++ util/scan.py
@@ -1,14 +1,12 @@
 import nmap
+from util.loggin import log_info, log_error
 
 def scan(ip, port='22'):
     nm = nmap.PortScanner()
     nm.scan(hosts=ip, ports=port)
     if nm.all_hosts():
-        print(f'Host encontrado! {ip}' )
+        log_info(f"Host encontrado: {ip}")
         for host in nm.all_hosts():
-            host_ip = host
-            #host_state = nm[host_ip].state()
-            #port_state = nm[host_ip]['tcp'][int(port)]['state']
-            return host_ip
+            return host
     else:
-        print(f'Host não encontrado! IP: {ip}')
+        log_error(f"Host não encontrado: IP {ip}")
--- util/snmp.py
+++ util/snmp.py
@@ -1,41 +1,39 @@
 from pysnmp.hlapi import *
 from pysnmp.entity.engine import SnmpEngine
+from util.loggin import log_info, log_error
 
-#mib = getMib()
-
-
-# validação:
-# entrada      ( )
-# processo     (X)
-# finalização  ( )
-
-def snmp(ip, listaSnmp = ''):
-
+def snmp(ip, listaSnmp=''):
     community = 'public'  # Comunidade SNMP configurada na impressora
-    if listaSnmp != '':
-        oids = listaSnmp
-    else:
-        oids = ['1.3.6.1.2.1.1.5.0', 'iso.3.6.1.2.1.43.5.1.1.17.1', 'iso.3.6.1.2.1.25.3.2.1.3.1', 'iso.3.6.1.2.1.43.10.2.1.4.1.1']
 
-    #print(f"\n Ip da impressora {ip}")
+    oids = listaSnmp if listaSnmp else [
+        '1.3.6.1.2.1.1.5.0',
+        'iso.3.6.1.2.1.43.5.1.1.17.1',
+        'iso.3.6.1.2.1.25.3.2.1.3.1',
+        'iso.3.6.1.2.1.43.10.2.1.4.1.1'
+    ]
+
+    log_info(f"Iniciando consulta SNMP para IP {ip}")
+
     iterator = getCmd(
-    SnmpEngine(),
-    CommunityData(community),
-    UdpTransportTarget((ip, 161), timeout=4, retries=1),
-    ContextData(),
-    *[ObjectType(ObjectIdentity(oid)) for oid in oids]
+        SnmpEngine(),
+        CommunityData(community),
+        UdpTransportTarget((ip, 161), timeout=4, retries=1),
+        ContextData(),
+        *[ObjectType(ObjectIdentity(oid)) for oid in oids]
     )
 
     error_indication, error_status, error_index, var_binds = next(iterator)
 
     if error_indication:
-        print(f"Erro SNMP: {error_indication}")
+        log_error(f"Erro SNMP em {ip}: {error_indication}")
         return []
     elif error_status:
-        print(f"Erro na resposta: {error_status.prettyPrint()}")
+        log_error(f"Erro na resposta SNMP em {ip}: {error_status.prettyPrint()}")
         return
+
     results = []
     for oid, val in var_binds:
         results.append({'oid': str(oid), 'resposta': str(val)})
+        log_info(f"OID {oid} → Resposta: {val}")
 
     return results
